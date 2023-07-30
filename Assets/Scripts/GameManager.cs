60ece74808d23cc45065d7e33747c49b518f3c51
Cagema <Cagema.erm@gmail.com>
2023-07-30 17:26:43 +0300

    Edit enemy animator

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,18 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public float TurnDelay = 0.1f;
     public static GameManager Instance = null;
     public BoardManager BoardScript;
     public int PlayerFoodPoints = 100;
     [HideInInspector] public bool PlayersTurn = true;
 
     private int _level = 1;
+    List<Enemy> _enemies;
+    bool _enemiesMoving;
 
     private void Awake()
     {
@@ -20,17 +22,44 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject);
 
         DontDestroyOnLoad(gameObject);
+        _enemies = new();
         BoardScript = GetComponent<BoardManager>();
         InitGame();
     }
+    private void Update()
+    {
+        if (PlayersTurn || _enemiesMoving) return;
 
+        StartCoroutine(MoveEnemies());
+    }
     private void InitGame()
     {
+        _enemies.Clear();
         BoardScript.SetupScene(_level);
     }
 
     public void GameOver()
     {
+        enabled = false;
+    }
 
+    public void AddEnemyToList(Enemy script)
+    {
+        _enemies.Add(script);
+    }
+    IEnumerator MoveEnemies()
+    {
+        _enemiesMoving = true;
+        yield return new WaitForSeconds(TurnDelay);
+        if (_enemies.Count == 0)
+        {
+            yield return new WaitForSeconds(TurnDelay);
+        }
+
+        for (int i = 0; i < _enemies.Count; i++)
+        {
+            _enemies[i].MoveEnemy();
+            yield return new WaitForSeconds(_enemies[i].MoveTime);
+        }
     }
 }
